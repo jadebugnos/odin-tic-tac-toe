72ad225e077806b2237074c7579742241f58ca98
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2025-02-18 22:18:29 +0800

    refactor player class to improve input validation
    - add occupied_cells variable to track occupied
    cells
    - reorder code placement to achieve desired
    behavior

--- lib/players.rb
+++ lib/players.rb
@@ -1,14 +1,25 @@
 # players.rb
 # this file defines the Player class which handles the player inputs
 class Player
+  def initialize
+    @occupied_cells = []
+  end
+
   def handle_player_input
     loop do
       number = Integer(gets.chomp)
-      return number if (1..9).include?(number)
 
-      puts "Invalid input! Please enter a number between 1 and 9"
+      if !(1..9).include?(number)
+        puts "Invalid input! Please enter a number between 1 and 9"
+      elsif @occupied_cells.include?(number)
+        puts "Cell already occupied, pick another one"
+        next
+      end
+
+      @occupied_cells.push(number)
+      return number
     rescue ArgumentError
-      puts "Invalid input! please enter a valid number"
+      puts "Invalid input! Please enter a valid number"
     end
   end
 
